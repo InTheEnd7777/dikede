fab88ceddf0ab0aecbe33015fb8884c144155be4
InTheEnd <InTheEnd7777@outlook.com>
2022-07-30 22:04:29 +0800

    login yemian

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en' // lang i18n
-
 import '@/styles/index.scss' // global css
 
 import App from './App'
@@ -14,7 +13,7 @@ import router from './router'
 
 import '@/icons' // icon
 import '@/permission' // permission control
-
+import '@/assets/iconfont/iconfont.css'
 /**
  * If you don't want to use mock-server
  * you want to use MockJs for mock api
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,10 +1,41 @@
 <template>
-  <div>
-    1
+  <div class="main">
+
+    <div class="login">
+      <div> <img src="@/assets/logo.595745bd.png" alt="" class="imgtubiao"></div>
+
+      <div>
+        <el-form ref="ruleForm" status-icon class="demo-ruleForm">
+          <el-form-item>
+            <el-input autocomplete="off" prefix-icon="el-icon-mobile-phone" />
+          </el-form-item>
+          <el-form-item>
+            <el-input autocomplete="off" prefix-icon="el-icon-lock"> <template #suffix>
+              <span class="iconfont icon-biyanjing" />
+            </template></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input autocomplete="off">
+              <template #prefix>
+                <span class="iconfont icon-mn_dunpai" />
+              </template>
+            </el-input>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="btnlogin">
+        <el-row>
+          <el-button class="q1">登录</el-button>
+        </el-row>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import '@/assets/iconfont/iconfont.css'
+
 export default {
   data() {
     return {
@@ -23,6 +54,42 @@ export default {
 </script>
 
 <style scoped>
-body{
-  background: url('../../assets/background.be4fae7d.png');}
+.main{
+  background: url('../../assets/background.be4fae7d.png') no-repeat center ;
+  background-size: cover;
+  height: 100%;}
+  .login{
+    position: absolute;
+    width: 518px;
+    height: 388px;
+    top: 50%;
+    left: 50%;
+    margin-top: -194px;
+    margin-left: -259px;
+    padding: 76px 35px 0;
+    background: #fff;
+    -webkit-box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+    box-shadow: 0 3px 70px 0 rgb(30 111 72 / 35%);
+    border-radius: 10px;
+  }
+  .btnlogin{
+        height: 52px;
+    background: linear-gradient(262deg,#2e50e1,#6878f0);
+
+  }
+    .q1{
+      width: 100%;
+              height: 52px;
+color: #fff;
+    background: linear-gradient(262deg,#2e50e1,#6878f0);
+
+    }
+    .imgtubiao{
+      position: absolute;
+    width: 96px;
+    height: 96px;
+    top: -46px;
+    left: 50%;
+    margin-left: -48px;
+    }
 </style>
