dd5b465444ab04eaeab62337d3a805bbab6d5c30
InTheEnd <InTheEnd7777@outlook.com>
2022-07-30 19:52:52 +0800

    初始化完成

---
+++ src/views/login/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    1
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  created() {
+
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+body{
+  background: url('../../assets/background.be4fae7d.png');}
+</style>
